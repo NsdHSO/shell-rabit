1fac6ee182411b61b839bd623cdd03f112f6bc36
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-06-08 09:57:25 +0300

    chore:🌻 added media query

--- src/app/modules/dashboard/state/dashboard.service.ts
+++ src/app/modules/dashboard/state/dashboard.service.ts
@@ -1,12 +1,11 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {environment} from "../../../../environments/environment";
 
 @Injectable({
-    providedIn: 'root'
+    providedIn : 'root'
 })
 export class DashboardService {
-    getCountRabbit$ = this._http.get(`${environment.apiProducts}/rabbit/cards`)
+    getCountRabbit$ = this._http.get(`${process.env['NX_API_URL']}/rabbit/cards`)
 
     constructor(private readonly _http: HttpClient) {
     }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
-import {bootstrapApplication} from '@angular/platform-browser';
+import {
+    bootstrapApplication
+} from '@angular/platform-browser';
 import {appConfig} from './app/app.config';
 import {AppComponent} from './app/app.component';
+import {enableProdMode} from "@angular/core";
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-    console.error(err)
-);
+if (process.env['NODE_ENV'] === 'production') {
+    enableProdMode();
+}
+
+
+bootstrapApplication(AppComponent,
+    appConfig)
+    .catch((err) => console.error(err));
